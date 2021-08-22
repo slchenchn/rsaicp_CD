d51bdf72a7c8c201d4bc9907dd9cf5d28801a1f7
chenda <1106982578@qq.com>
2021-08-22 19:41:05 +0800

    nonoe

--- multi_trails.sh
+++ multi_trails.sh
@@ -1,4 +1,8 @@
 source env.sh
-python tools/train.py configs/siamunet/unet_512x512_40k_s2looking.py 
+# python tools/train.py configs/siamunet/unet_512x512_40k_s2looking.py 
 # python tools/train.py configs/siamunet/unet_512x512_20k_s2looking.py \
     # --options optimizer.lr=1e-5
+
+export CUDA_VISIBLE_DEVICES=1,2,3
+
+
